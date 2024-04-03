31102546a6074470a73f878eba4606290e4935bb
james <james.bardowski@gmail.com>
2024-04-03 07:08:45 -0400

    Skipping files if already parsed.

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -13,15 +13,15 @@ def extract(file_path):
     return text
 
 
-# def save_text_to_file(file_path):
-#     with open(file_path, 'w') as file:
-#         file.write(text)
-
-
 def main_extract(raw_dir='./raw_docs', parsed_dir='./parsed_docs'):
-    # loop all pdf files in pdf_docs_raw, call extract function, then save to txt file in pdf_docs_parsed
+    # loop all files in pdf_docs_raw, call extract function if not already parsed, then save to txt file in parsed_dir.
+    already_parsed = os.listdir(parsed_dir)
+
     for filename in os.listdir(raw_dir):
         if filename.endswith(".pdf"):
+            if filename.replace(".pdf", ".txt") in already_parsed:
+                print(f"{filename} already parsed, skipping.")
+                break
             print(f"About to parse {filename}...")
             f_path = os.path.join(raw_dir, filename)
             text = extract(f_path)
